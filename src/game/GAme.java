96738134618d4417e4bc17796a9afbd4e016cd5c
Hanner <hanner.sinisterra@correounivalle.edu.co>
2023-04-20 12:46:04 -0500

    carga inicial

---
+++ src/game/GAme.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package game;
+
+/**
+ *
+ * @author ivan
+ */
+
+
+public class GAme {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        JuegoAnimales m=new JuegoAnimales();
+        m.jugar();
+    }
+
+    
+    
+}
