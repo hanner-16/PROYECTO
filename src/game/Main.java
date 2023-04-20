6fc4106517ec13bead254b91912adefcfa599a1f
Hanner <hanner.sinisterra@correounivalle.edu.co>
2023-04-20 13:20:47 -0500

    se actualizo clase "Main"

--- src/game/Main.java
+++ src/game/Main.java
@@ -5,20 +5,15 @@
 package game;
 
 /**
- *
- * @author Hanner Sinisterra
- * @author Daniel Gallego
+ * @author Hanner Fernando Sinisterra cod: 2122993-3743
+ * @author Daniel Esteban Gallego cod: 2124305 -3743
  */
 
 
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
 
+    public static void main(String[] args) {   
         JuegoLetras m=new JuegoLetras();
         m.jugar();
     }
